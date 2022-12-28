7a04e6e4bdb3fa2586fe600011094997da9d3c45
Brian Denmark <testingarea479@gmail.com>
2022-12-28 11:37:06 -0800

    added model and css

--- src/main/java/com/briandenmark/ReceiptController.java
+++ src/main/java/com/briandenmark/ReceiptController.java
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ReceiptController {
 	@RequestMapping("/")
 	public String index(Model model) {
+		
+		model.addAttribute("name", "Grace Hopper");
+		model.addAttribute("itemName", "Copper Wire");
+		model.addAttribute("price", "5.25");
+		model.addAttribute("description", "Metal strips, also an illustration of nanoseconds.");
+		model.addAttribute("vendor","Little Things Corner Store");
+		
 		return "index.jsp";
 	}
 }
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -11,6 +11,12 @@
 <title>MIT Wires Talk</title>
 </head>
 <body>
-	<h1>We made it boi! On god! frfr</h1>
+	<h1 style="font-size: 3rem; font-weight: bold;"><c:out value="${vendor}"/></h1>
+	<ul style="list-style-type:none;">
+		<li style="font-size: 1.25rem; font-weight: bold;">Customer Name: <c:out value="${name}"/></li>
+		<li>Item: <c:out value="${itemName}"/></li>
+		<li>Price: <c:out value="${price}"/></li>
+		<li>Description: <c:out value="${description}"/></li>
+	</ul>	
 </body>
 </html>
